a83669a5a99da86518b3792a02e6418fc18f8b63
Benjamin David Walcott <benjamin.walcott@gmail.com>
2018-03-05 10:31:50 -0500

    skeleton of program written

---
+++ seamTest.py
@@ -0,0 +1,15 @@
+#/bin/env python
+import subprocess
+import sys
+import argparse
+import glob
+
+def main():
+    # getchain
+    # renumber
+    # 3to1
+    # pdb2cij
+    # pdb2hb
+    # contactmask
+    # seams
+    # cleanup
