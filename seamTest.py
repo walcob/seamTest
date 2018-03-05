cf8c734054499cfe7584b74a887eb22f24335996
Benjamin David Walcott <benjamin.walcott@gmail.com>
2018-03-05 14:46:11 -0500

    seamTest code written

--- seamTest.py
+++ seamTest.py
@@ -4,22 +4,26 @@ import sys
 import argparse
 import glob
 
-def cleanup():
-    None
-    
-def writeParamaters(parfile):
-    None
+def cleanup(pdb):
+    subprocess.run(["rm","-v","%s_1.pdb"%(pdb),"%s.hb"%(pdb)],stdout=sys.stdout)
 
 def main():
     # Get list of pdbs
     barrels = glob.glob("database/barrels/*")
     nonBarrels = glob.glob("database/nonbarrels/*")
-    # getchain
-    # renumber
-    # 3to1
-    # pdb2cij
-    # pdb2hb
-    # contactmask
-    # seams
-    # cleanup
-    None
+    pdbs = barrels+nonBarrels
+    for pdb in pdbs:
+        # parameters
+        
+        # renumber
+        subprocess.run(["GeoFold/xrenumber_one",pdb,"%s_1.pdb"%(pdb)],stdout=sys.stdout)
+        # pdb2hb
+        hbfile = open("%s.hb"%(pdb),"w+")
+        subprocess.run(["GeoFold/xpdb2hb","default.par","%s_1.pdb"%(pdb)],stdout=hbfile,stderr=sys.stdout)
+        hbfile.close()
+        # seams
+        seamfile = open("%s.seams"%(pdb),"w+")
+        subprocess.run(["GeoFold/seams/xpdb2seams2","%s.hb"%(pdb)],stdout=seamfile,stderr=sys.stdout)
+        seamfile.close()
+        # cleanup
+        cleanup(pdb)
