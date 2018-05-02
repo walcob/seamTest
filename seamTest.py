8f1a63815bcc7e8a74493acff57fe0ae133fb10b
Benjamin David Walcott <benjamin.walcott@gmail.com>
2018-05-01 23:59:19 -0400

    cleaned up debug code and added more comments

--- seamTest.py
+++ seamTest.py
@@ -1,4 +1,7 @@
 #/bin/env python
+"""
+This python script runs test for the new seam implementation in GeoFold, pdb2seams2.
+"""
 import subprocess
 import sys
 import argparse
@@ -8,30 +11,33 @@ import os.path
 
 
 def cleanup(basename,log):
+    '''Clean up temporary files generated in a run of seamtest'''
+    # Log can either be to sys.stdout or a file
     openedLog = False
     if log != sys.stdout and type(log) == str:
         log = open(log,'ab')
         openedLog = True
+    # delete the files
     subprocess.run(["rm","-v","%s_1.pdb"%(basename),"%s.hb"%(basename)],stdout=log)
     if openedLog: log.close()
 
 def runTest(pdb,log):
+    '''This is the meat of the program.  Given a pdb file, performs preprocessing
+    necessary for pdb2seams2 and performs pdb2seams2 barrel detection.'''
+    # Deal with the logfile
     openedLog = False
     if log != sys.stdout and type(log) == str: 
         log = open(log,'ab')
         openedLog = True
+    # get PDB code
     basename = os.path.basename(pdb)[:-4]
-    # print("="*25,basename,"="*25)
-    # renumber
-    # print("="*25,"Renumbering","="*25)
+    # renumber, number the pdb sequentially
     subprocess.run(["GeoFold/xrenumber_one",pdb,"%s_1.pdb"%(basename)],stdout=log)
-    # pdb2hb
-    # print("="*25,"Extracting H-bonds","="*25)
+    # pdb2hb, find hydrogen bonds
     hbfile = open("%s.hb"%(basename),"w+")
     subprocess.run(["GeoFold/xpdb2hb","default.par","%s_1.pdb"%(basename)],stdout=hbfile,stderr=log)
     hbfile.close()
-    # seams
-    # print("="*25,"Finding Seams","="*25)
+    # seams, perform pdb2seams2 barrel detection
     seamfile = open("seamfiles/%s.seams"%(basename),"w+")
     subprocess.run(["GeoFold/seams/xpdb2seams2","%s.hb"%(basename)],stdout=seamfile,stderr=log)
     seamfile.close()
@@ -39,29 +45,36 @@ def runTest(pdb,log):
     return basename
 
 def makepar(pdb,parfile):
+    '''Generates parameters file, parfile, for a full run of GeoFold on pdb'''
     fout = open(parfile,'w+')
     fout.write("LNAME %s\n"%(os.path.basename(pdb)))
     fout.write("EMAIL walcob@rpi.edu\n")
     fout.write("PDBCODE %s\n"%(os.path.basename(pdb)[:-4]))
-    fout.write("OMEGA 1.\nINTERMEDIATE 0\nBARRELMOVES 1\nORANGE 0. 0.5 1.0 1.5\nRUNGEOFOLD 1\nMOLSCRIPT 0\nBREAKCUT 0.05\nPIVOTCUT 0.25\nHINGECUT 0.5\nSEAMCUT 10\nBREAKPOINTENTROPY 90.\nHINGEPOINTENTROPY 30.\nTEMPERATURE 300.\nCONCENTRATION 1.\nVOIDENTROPY 0.\nSOLIDITY 1000.\nHBONDENERGY 100.\nHAMMONDSCALE 1000.\nSIDECHAINENTROPY 1.\nHINGEBARRIER 0.\nPIVOTBARRIER 0.\nWATER 1.\nMAXSPLIT 4\nMAXTIME 10.\nMINSEG 4\nCAVITATION 0.000001\nFLORY 0\nSUBMIT submit\nREDUCING 0\nMOLSCRIPT 1\nHLFE 0\nFING 0\nCHAIN +\nwc 1.\nwn 8.\n")
+    fout.write("OMEGA 1.\nINTERMEDIATE 0\nBARRELMOVES 1\nORANGE 0. 0.5 1.0 1.5\nRUNGEOFOLD 1\nMOLSCRIPT 0\nBREAKCUT 0.05\nPIVOTCUT 0.25\nHINGECUT 0.5\nSEAMCUT 10\nBREAKPOINTENTROPY 90.\nHINGEPOINTENTROPY 30.\nTEMPERATURE 300.\nCONCENTRATION 1.\nVOIDENTROPY 0.\nSOLIDITY 1000.\nHBONDENERGY 100.\nHAMMONDSCALE 1000.\nSIDECHAINENTROPY 1.\nHINGEBARRIER 0.\nPIVOTBARRIER 0.\nWATER 1.\nMAXSPLIT 4\nMAXTIME 10.\nMINSEG 4\nCAVITATION 0.000001\nFLORY 0\nSUBMIT submit\nREDUCING 0\nMOLSCRIPT 1\nHLFE 0\nFOLDING 0\nCHAIN +\nwc 1.\nwn 8.\n")
     fout.close()
 
 def fullTest(pdb,parfile,log):
+    '''Performs full run of GeoFold given parfile parameters file and pdb'''
+    # logfile handling
     openedLog = False
     if log != sys.stdout and type(log) == str: 
         log = open(log,'ab')
         openedLog = True
+    # create the parameters file
     makepar(pdb,parfile)
+    # run GeoFold
     subprocess.run(["python","GeoFold/rungeofold.py",parfile,"/Users/walcob/seamTest/seamTest.conf"],stdout=log,stderr=log)
     if openedLog: log.close()
     
 def getSCOPe(pdb):
-    None
+    '''Extracts the SCOPe structural class from a given pdb'''
     with open(pdb) as pdbIn:
         lines = "\n".join([line for line in pdbIn if "REMARK  99 ASTRAL SCOPe-sccs:" in line])
     return lines.split(":")[1].strip()
     
 def findBarrel(basename):
+    '''Check pdb2seams2 output for barrels'''
+    # Find the line in the output showing the number of barrels detected
     with open("seamfiles/%s.seams"%(basename)) as seamIn:
         line = "".join([line for line in seamIn if line[:8] == "NBARRELS"])
         print(line);sys.stdout.flush()
@@ -72,6 +85,7 @@ def findBarrel(basename):
             return False
     
 def SCOPeTest():
+    '''Detect barrels in entire SCOPe database'''
     # MPI setup to make this faster
     from mpi4py import MPI
     comm = MPI.COMM_WORLD
@@ -114,57 +128,79 @@ def SCOPeTest():
     writeSCOPe(barrels,"SCOPeBarrels_%i.txt"%(rank),nonbarrels,"SCOPeNonbarrels_%i.txt"%(rank))
 
 def writeSCOPe(barrels,barrelsout,nonbarrels,nonbarrelsout):
+    '''Write out SCOPe domains detected as barrels and nonbarrels'''
     bout = open(barrelsout,"w+")
     nout = open(nonbarrelsout,"w+")
+    # for every SCOPe ID, write the id and all entries of that class identified as barrels
     for SCOPe in sorted(barrels.keys()):
         bout.write("%s\n"%(SCOPe))
         bout.write("\n".join(barrels[SCOPe]))
         bout.write("\n")
     bout.close()
+    # like above, but for nonbarrels
     for SCOPe in sorted(nonbarrels.keys()):
         nout.write("%s\n"%(SCOPe))
         nout.write("\n".join(nonbarrels[SCOPe]))
         nout.write("\n")
     nout.close()
     
-def doFullTests(log):
+def doFullTests(log,pdbs=['database/barrels/2B3P.pdb','database/barrels/2AWJ.pdb','database/barrels/1I1B.pdb','database/barrels/1R2T.pdb','database/barrels/1JOO.pdb']):
+    '''Perform full runs of GeoFold so pathway prediction accuracy may be assessed.
+    Only perform this for entries with experimental folding data.'''
     # MPI setup to make this faster
     from mpi4py import MPI
     comm = MPI.COMM_WORLD
     rank = comm.Get_rank()
     size = comm.Get_size()
+    # logfile parsing
     if log != sys.stdout: log = open("%s_%i.%s"%(log.split('.')[0],rank,log.split('.')[1]),'ab')
-    # load pdbs
-    pdbs = ['database/barrels/2B3P.pdb','database/barrels/2AWJ.pdb','database/barrels/1I1B.pdb','database/barrels/1R2T.pdb','database/barrels/1JOO.pdb']
+    # load pdbs and run GeoFold
     for i in range(rank,len(pdbs),size):
         print(i,rank,pdbs[i]);sys.stdout.flush()
         fullTest(pdbs[i],"%s.par"%(os.path.basename(pdbs[i])[:-4]),log)
-        
+    # close log
     if log != sys.stdout: log.close()
     
 def main():
-    # Get list of pdbs
+    # parser setup
     parser = argparse.ArgumentParser(description="Test seams")
+    # run on all pdbs in database directory
     parser.add_argument("--all",action="store_true",default=False)
+    # use debug output
     parser.add_argument("--debug",action="store_true")
+    # files to test
     parser.add_argument("-f",nargs='+')
+    # run on the entire SCOPe database
     parser.add_argument("--SCOPe",action="store_true",default=False)
+    # do a full run of GeoFold
     parser.add_argument("--full",action="store_true",default=False)
+    # logfile
     parser.add_argument("-log",default=None,help="log output to logfile. set to logfile path")
+    
+    # parse arguments
     args = parser.parse_args()
     pdbs = []
+    # logfile setup
     if args.log:
         log = args.log
     else:
         log = sys.stdout
+    # test full SCOPe
     if args.SCOPe:
         SCOPeTest()
+    # perform full GeoFold test
     elif args.full:
-        doFullTests(log)
+        if args.f is None:
+            doFullTests(log)
+        else:
+            doFullTests(log,args.f)
     else:
+        # check full database
         if(args.all):
             pdbs += glob.glob("database/barrels/*") + glob.glob("database/nonbarrels/*")
+        # check specified files
         if(args.f is not None): pdbs += args.f
+        # run the tests
         for pdb in pdbs:
             basename = runTest(pdb,log)
             # cleanup
